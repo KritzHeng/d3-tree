e92b707208f593030154bebe4b786453915d707a
KritzHeng <mr.kriskanin@gmail.com>
2022-08-14 21:57:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import Tree from "react-d3-tree";
+import { RawNodeDatum } from "react-d3-tree/lib/types/common";
+import React, { useState } from "react";
+// import React from 'react';
+import Tree from "react-d3-tree";
+import "./custom-tree.css";
+const orgChart = {
+  name: "CEO",
+  children: [
+    {
+      name: "Manager",
+      attributes: {
+        department: "Production",
+      },
+      children: [
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Assembly",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
-function App() {
+export default function OrgChartTree() {
+  // const [tree, setTree] = useState<RawNodeDatum >({
+  //   name: "Root",
+  //   children: [],
+  // })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div id="treeWrapper" style={{ width: "50em", height: "20em" }}>
+    //   <Tree data={orgChart} />
+    // </div>
+    <div id="treeWrapper" style={{ width: "500em", height: "200em" }}>
+      <Tree
+        data={orgChart}
+        rootNodeClassName="node__root"
+        branchNodeClassName="node__branch"
+        leafNodeClassName="node__leaf"
+      />
     </div>
   );
 }
-
-export default App;
